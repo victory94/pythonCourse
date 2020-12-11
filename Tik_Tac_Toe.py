9b469999562910eb13b5f0e6075760a4306ebb2e
Viktoria <viktoria_a94@hotmail.com>
2020-12-11 20:20:13 +0100

    Implemented check of winner with the help og "magic square".

--- Tik_Tac_Toe.py
+++ Tik_Tac_Toe.py
@@ -1,16 +1,40 @@
 
 
 current_player = {"current_player": "X"}
+magic_square = [[8, 3, 4], [1, 5, 9], [6, 7, 2]]
 
 
 # Check if current_player has won the board.
 # Returns True if won, False otherwise
 def check_winner(board, current_player):
-    win_counter = 0
-    #TODO: go through borad checing for winner
+    is_winner = False
+    magic_square_calc = 0
 
+    # Checks if current_player has won the diagonal
+    for diag in range(3):
+        if board[diag][diag] == current_player:
+            magic_square_calc += magic_square[diag][diag]
+        if magic_square_calc == 15:
+            return True
 
-    return False
+    magic_square_calc = 0
+    # Checks reverse diagonal
+    for diag in range(3):
+        if board[diag-2][diag] == current_player:
+            magic_square_calc += magic_square[diag][diag]
+        if magic_square_calc == 15:
+            return True
+
+    magic_square_calc = 0
+    # Checks if winner is in single row or single column
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == current_player:
+                magic_square_calc += magic_square[row][col]
+            if magic_square_calc == 15:
+                return True
+    print(magic_square_calc)
+    return is_winner
 
 
 def display(matrix):
@@ -42,7 +66,9 @@ def user_input_col():
 
 
 if __name__ == '__main__':
-    tic_tac_toe = [["-" for col in range(3)]for row in range(3)]
+    tic_tac_toe = [[None for col in range(3)]for row in range(3)]
+    check_winner(tic_tac_toe, "X")
+
     game_close = "N"
     while game_close != "Y":
         print('\n'*100)
@@ -50,14 +76,14 @@ if __name__ == '__main__':
         print(f'Välj ruta för {current_player["current_player"]}')
         row_number = int(user_input_row())
         col_number = int(user_input_col())
-        if tic_tac_toe[row_number][col_number] != "-":
+        if tic_tac_toe[row_number][col_number] is not None:
             print("Ruta upptagen, välj annan")
             continue
         else:
             tic_tac_toe[row_number][col_number] = current_player["current_player"]
             if check_winner(tic_tac_toe, current_player["current_player"]):
-                print(current_player["current_player"] + str(" har vunnit!"))
                 display(tic_tac_toe)
+                print(str(current_player["current_player"]) + str(" har vunnit!"))
                 game_close = "Y"
                 break
             # Checks for -, since we start with board full of -
