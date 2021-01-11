61fb4d2f990c5bd83716d3f2d5d75d39386cb2de
Viktoria <viktoria_a94@hotmail.com>
2021-01-11 20:28:38 +0100

    Created python file for challenge of OO chapter of python course. COntains class Account.

---
+++ OOChallenge/Account.py
@@ -0,0 +1,17 @@
+#For this challenge, create a bank account class that has two attributes:
+#    owner
+#   balance
+#and two methods:
+#   deposit
+#   withdraw
+#As an added requirement, withdrawals may not exceed the available balance.
+
+#Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
+
+
+class Account:
+    pass
+
+
+if __name__ == '__main__':
+    pass
