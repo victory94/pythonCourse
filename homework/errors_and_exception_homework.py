72cab9c73298e7e0cd15df4430d76d9d9e33ef2f
Viktoria <viktoria_a94@hotmail.com>
2021-01-13 18:53:04 +0100

    Added .py file for error handling homework section of course, with dummy code for problems.

---
+++ homework/errors_and_exception_homework.py
@@ -0,0 +1,35 @@
+# Handle the exception thrown by the code below by using try and except blocks.
+def problem1():
+    for i in ['a', 'b', 'c']:
+        print(i ** 2)
+    # TODO: Add try catch statement
+    pass
+
+
+# Handle the exception thrown by the code below by using try and except blocks.
+# Then use a finally block to print 'All Done.'
+def problem2():
+    x = 5
+    y = 0
+
+    z = x / y
+
+    # TODO: Add try catch statement
+    pass
+
+
+# Write a function that asks for an integer and prints the square of it.
+# Use a while loop with a try, except, else block to account for incorrect inputs.
+def ask():
+    # TODO: Add while loop
+    # TODO: Ask for integer
+    # TODO: print square of integer
+    # TODO: Add try except else block to account for incorrect inputs (TypeError?)
+    pass
+
+
+if __name__ == '__main__':
+    problem1()
+    problem2()
+    ask()
+
